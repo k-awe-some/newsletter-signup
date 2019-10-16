2ae68152edd1f16e7cd3a53d64b677a2dcbea97d
kay <kaynguyen.dev@gmail.com>
2019-10-16 16:34:30 -0400

    config mailchimp to post data to mailchimp database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
+const keys = require("./config/keys");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,6 +11,39 @@ app.get("/", (req, res) => res.render("index"));
 app.post("/", (req, res) => {
   const { firstName, lastName, email } = req.body;
   console.log(firstName, lastName, email);
+  const data = {
+    // parameters defined by MailChimp
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+  const JSONdata = JSON.stringify(data);
+
+  const options = {
+    url: `https://${keys.mailchimpServer}.api.mailchimp.com/3.0/lists/${keys.mailchimpListID}`,
+    method: "POST",
+    headers: {
+      Authorization: `${keys.mailchimpUsername} ${keys.mailchimpKey}`
+    },
+    body: JSONdata
+  };
+
+  request(options, (error, response, body) => {
+    if (error) {
+      res.render("failure");
+    } else {
+      response.statusCode === 200
+        ? res.render("success")
+        : res.render("failure");
+    }
+  });
 });
 
 app.listen(3000, () => console.log("🎉 Server is running on port 3000"));
